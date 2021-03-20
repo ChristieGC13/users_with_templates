6fb7b94b25090e7bf47eb4e5f9617328bbc1bf94
Christie Caiano <christiecaiano@gmail.com>
2021-03-20 18:01:44 -0400

    first commit

---
+++ users_app/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+from .models import User
+
+# Create your views here.
+
+def root(request):
+    context = {
+        "all_the_users" : User.objects.all()
+    }
+    return render(request, "index.html", context)
+
+def add(request):
+    User.objects.create(
+        first_name=request.POST['first_name'],
+        last_name=request.POST['last_name'],
+        email=request.POST['email'],
+        age=request.POST['age']
+    )
+    return redirect('/')
