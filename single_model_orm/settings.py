4c06138c42692853b4f1b619731266ac5f799180
Christie Caiano <59705609+ChristieGC13@users.noreply.github.com>
2021-03-20 18:20:28 -0400

    Update settings.py

--- single_model_orm/settings.py
+++ single_model_orm/settings.py
@@ -22,9 +22,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'u-)kj*n%kqh=6b8l*&kn@hzo7_7h0!95gmm9*grfd(7-#x(2@a'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
